b0047981139a9bb3f5242e1e9183df910dc95e1f
Filoupegase <corentin.loison@omnilog>
2020-10-04 22:38:27 +0200

    new product page

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ fetch(apiURL)
         }
     })
     .then(data => {
-        data.forEach(objet => {
+        data.forEach(data => {
 
-            let priceProd = objet.price / 100;
+            let priceProd = data.price / 100;
 
             god.innerHTML += `
+             <a href="detail.html?id=${data._id}">
             <div class="max-w-sm parent rounded overflow-hidden shadow-lg bg-white cursor-pointer">
-            <img class="w-full parent child" src="${objet.imageUrl}" alt="${objet.name}">
+            <img class="w-full parent child" src="${data.imageUrl}" alt="${data.name}">
             <div class="px-6 py-4">
-                <div class="font-bold text-2xl mb-2">${objet.name}</div>
+                <div class="font-bold text-2xl mb-2">${data.name}</div>
                 <div class="text-lg mb-2">${priceProd.toFixed(2)} €</div>
-                <p class="text-gray-700 text-base">${objet.description}</p>
+                <p class="text-gray-700 text-base">${data.description}</p>
             </div>
             <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Teddies</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Pink</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Orinoco</span>
             </div>
-        </div>`; //j'injecte mon HTML avec les bonnes variables directement dans le DOM
-            console.log(objet);
+        </div>
+        </a>`; //j'injecte mon HTML avec les bonnes variables directement dans le DOM
+            console.log(data);
         })
     }).catch(error => alert("Erreur : " + error));
 /*
@@ -39,10 +41,10 @@ fetch("http://localhost:3000/api/teddies/")
     .catch(error => alert("Erreur : " + error));
 */
 /*
-
 fetch(apiURL)
     .then(response=>response.json())
     .then(data=>{
         console.log(data);
     });
-console.log(apiURL);*/
+console.log(apiURL);
+*/
