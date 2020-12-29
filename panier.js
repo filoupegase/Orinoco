ca057a929866a96c3c115ea79b6456bd2740be68
Filoupegase <corentin.loison@omnilog>
2020-12-29 18:54:51 +0100

    feat array basket

--- panier.js
+++ panier.js
@@ -5,7 +5,7 @@ const form = document.getElementById('form')
 //const { v4: uuidv4 } = require('uuid');
 
 let data = JSON.parse(localStorage.getItem('basket'));
-if (data.length === 0) {
+if (data === 0 || data === null) {
     emptybasket.innerHTML += `
 <p class="mb-20 text-center mt-20 text-gray-600 text-4xl font-bold">Votre panier est vide :'(</p>
 `;
@@ -110,113 +110,114 @@ if (data.length === 0) {
             <div class="md:w-2/3"></div>
         </div>
     </form>`;
-    }
+
 
 //-- fonction de suppression d'un produit
-    function deleteItem(_id) {
-        let supprItem = JSON.parse(localStorage.getItem("basket"));
-        const item = supprItem.find(item => item.idProd == _id)
-        const index = supprItem.indexOf(item);
-        supprItem.splice(index, 1);
-        localStorage.setItem("basket", JSON.stringify(supprItem));
-        alert('Vous avez supprimé ' + item.name + ' de votre panier ! ')
-        document.location.href = 'panier.html';
-    }
+        function deleteItem(_id) {
+            let supprItem = JSON.parse(localStorage.getItem("basket"));
+            const item = supprItem.find(item => item.idProd == _id)
+            const index = supprItem.indexOf(item);
+            supprItem.splice(index, 1);
+            localStorage.setItem("basket", JSON.stringify(supprItem));
+            alert('Vous avez supprimé ' + item.name + ' de votre panier ! ')
+            document.location.href = 'panier.html';
+        }
 
 // ------------FORMULAIRE--------------
-    document.forms["form"].addEventListener("submit", function (e) {
-        e.preventDefault(); // <- (e) gérer le comportement du formulaire
-        var erreur;
-        var allInputs = this;
+        document.forms["form"].addEventListener("submit", function (e) {
+            e.preventDefault(); // <- (e) gérer le comportement du formulaire
+            var erreur;
+            var allInputs = this;
 
-        // Traitement générique pour le cas ou la totalité des champs serait vide
-        for (var i = 0; i < allInputs.length; i++) {
-            if (!allInputs[i].value) {
-                erreur = "Veuillez renseigner tous les champs";
-                break;
+            // Traitement générique pour le cas ou la totalité des champs serait vide
+            for (var i = 0; i < allInputs.length; i++) {
+                if (!allInputs[i].value) {
+                    erreur = "Veuillez renseigner tous les champs";
+                    break;
+                }
             }
-        }
 
-        let nomRegExp = new RegExp("^[a-zA-ZÀ-ú-s]+$");
-        let erreurNom;
-        document.getElementById("aideNom").textContent = "";
-        if (!nomRegExp.test(allInputs["nom"].value)) {
-            erreurNom = "erreur sur le nom";
-            document.getElementById("aideNom").textContent = erreurNom;
-            return false;
-        }
+            let nomRegExp = new RegExp("^[a-zA-ZÀ-ú-s]+$");
+            let erreurNom;
+            document.getElementById("aideNom").textContent = "";
+            if (!nomRegExp.test(allInputs["nom"].value)) {
+                erreurNom = "erreur sur le nom";
+                document.getElementById("aideNom").textContent = erreurNom;
+                return false;
+            }
 
-        let preRegExp = new RegExp("^[a-zA-ZÀ-ú-s]+$");
-        let erreurPre;
-        document.getElementById("aidePrenom").textContent = "";
-        if (!preRegExp.test(allInputs["prenom"].value)) {
-            erreurPre = "erreur sur le prénom";
-            document.getElementById("aidePrenom").textContent = erreurPre;
-            return false;
-        }
+            let preRegExp = new RegExp("^[a-zA-ZÀ-ú-s]+$");
+            let erreurPre;
+            document.getElementById("aidePrenom").textContent = "";
+            if (!preRegExp.test(allInputs["prenom"].value)) {
+                erreurPre = "erreur sur le prénom";
+                document.getElementById("aidePrenom").textContent = erreurPre;
+                return false;
+            }
 
-        let adrRegExp = new RegExp("^[a-zA-ZÀ-ú-s0-9_ ]+$");
-        let erreurAdr;
-        document.getElementById("aideAdresse").textContent = "";
-        if (!adrRegExp.test(allInputs["adresse"].value)) {
-            erreurAdr = "erreur sur l'adresse";
-            document.getElementById("aideAdresse").textContent = erreurAdr;
-            return false;
-        }
+            let adrRegExp = new RegExp("^[a-zA-ZÀ-ú-s0-9_ ]+$");
+            let erreurAdr;
+            document.getElementById("aideAdresse").textContent = "";
+            if (!adrRegExp.test(allInputs["adresse"].value)) {
+                erreurAdr = "erreur sur l'adresse";
+                document.getElementById("aideAdresse").textContent = erreurAdr;
+                return false;
+            }
 
-        let codRegExp = new RegExp("^(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}$");
-        let erreurCod;
-        document.getElementById("aidePostCode").textContent = "";
-        if (!codRegExp.test(allInputs["postCode"].value)) {
-            erreurCod = "erreur sur le code postal";
-            document.getElementById("aidePostCode").textContent = erreurCod;
-            return false;
-        }
+            let codRegExp = new RegExp("^(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}$");
+            let erreurCod;
+            document.getElementById("aidePostCode").textContent = "";
+            if (!codRegExp.test(allInputs["postCode"].value)) {
+                erreurCod = "erreur sur le code postal";
+                document.getElementById("aidePostCode").textContent = erreurCod;
+                return false;
+            }
 
-        let mailRegExp = new RegExp("^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$");
-        let erreurMail;
-        document.getElementById("aideCourriel").textContent = "";
-        if (!mailRegExp.test(allInputs["email"].value)) {
-            erreurMail = "erreur sur le mail";
-            document.getElementById("aideCourriel").textContent = erreurMail;
-            return false;
-        }
+            let mailRegExp = new RegExp("^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$");
+            let erreurMail;
+            document.getElementById("aideCourriel").textContent = "";
+            if (!mailRegExp.test(allInputs["email"].value)) {
+                erreurMail = "erreur sur le mail";
+                document.getElementById("aideCourriel").textContent = erreurMail;
+                return false;
+            }
 
-        const ids = [];
-        for (const product of data) {
-            ids.push(product.idProd)
-        }
-        //console.log(ids);
-        const user = {
-            lastName: allInputs["nom"].value,
-            firstName: allInputs["prenom"].value,
-            address: allInputs["adresse"].value,
-            city: allInputs["postCode"].value,
-            email: allInputs["email"].value
-        };
-        //console.log(user);
+            const ids = [];
+            for (const product of data) {
+                ids.push(product.idProd)
+            }
+            //console.log(ids);
+            const user = {
+                lastName: allInputs["nom"].value,
+                firstName: allInputs["prenom"].value,
+                address: allInputs["adresse"].value,
+                city: allInputs["postCode"].value,
+                email: allInputs["email"].value
+            };
+            //console.log(user);
 
 
-        const donnees = {products: ids, contact: user};
-        //console.log(donnees);
+            const donnees = {products: ids, contact: user};
+            //console.log(donnees);
 
-        const options = {
-            method: "POST",
-            body: JSON.stringify(donnees),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-        //console.log(options);
+            const options = {
+                method: "POST",
+                body: JSON.stringify(donnees),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            //console.log(options);
 
-        fetch("http://localhost:3000/api/teddies/order", options)
-            .then((response) => {
-                return response.json();
-            })
-            .then(function (order) {
-                window.location.href = `contact.html?ncomm=${order.orderId}`;
-            }).catch(error => {
-            return alert("Erreur : " + error)
-        });
-    })
+            fetch("http://localhost:3000/api/teddies/order", options)
+                .then((response) => {
+                    return response.json();
+                })
+                .then(function (order) {
+                    window.location.href = `contact.html?ncomm=${order.orderId}`;
+                }).catch(error => {
+                return alert("Erreur : " + error)
+            });
+        })
+    }
 }
